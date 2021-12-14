521cb3e66dfb85566c6717981cf3cec825b8a619
Jenny Chau <jnechau@gmail.com>
2021-12-13 21:07:23 -0500

    commit tree 30c66698129a27cbbad1d0cb825933188b0bfcff

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed
+    def initialize (name, breed)
+        @name = name
+        @breed = breed
+    end 
 end
+
+d1 = Dog.new("Bern", "Mastiff")
+puts d1.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
 
+    attr_accessor :name
+    attr_accessor :job
+
+    def initialize (name, job)
+        @name = name
+        @job = job
+    end 
+
 end
+
+p1 = Person.new("Jenny", "student")
+p p1.name
+p p1.job
